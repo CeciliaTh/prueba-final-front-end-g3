555239255b53bcb0f3037bc38b8008914922a06f
Carlos Garcia <davidgarciap13@gmail.com>
2023-04-16 00:56:20 -0400

    funcionalidad vista perfil

--- src/Views/Detalle.jsx
+++ src/Views/Detalle.jsx
@@ -25,73 +25,72 @@ const Detalle = () => {
   // }
 
   const comentar = e => {
-     e.preventDefault()
-     const date = new Date()
+    e.preventDefault()
+    const date = new Date()
     const objetComent = {
       comentario: comentario,
       id: Date.now(),
       idBurger: Number(id),
       burgerName: burgerDetalle.nombre,
       idUser: usuarioActual.id,
-      date: date.getDay() + '-' + Number(date.getMonth()+1) + '-' + date.getFullYear() + ' | ' + date.getHours() + ':' + date.getMinutes().toPrecision(),
-      userName: usuarioActual.nombre 
+      date: date.getDay() + '-' + Number(date.getMonth() + 1) + '-' + date.getFullYear() + ' | ' + date.getHours() + ':' + date.getMinutes().toPrecision(),
+      userName: usuarioActual.nombre
     }
     usuarioActual.comentarios.push(objetComent)
-    setUsuarioActual({...usuarioActual})
+    setUsuarioActual({ ...usuarioActual })
     setComentarios([objetComent, ...comentarios])
-    console.log(comentarios)
     document.querySelector('#textareacomentario').value = '';
   }
 
   return (
     <div>
-    <div className='detalle-container'>
-      <div className="card-detalle" >
-        <div className="cardimg-detalle">
-          <img src={burgerDetalle.img} alt="imagen-hamburguesa" className="imagen"></img>
-        </div>
-        <h4>
-          {burgerDetalle.nombre}
-        </h4>
-        <p className="desc"> {burgerDetalle.desc}</p>
-        <p> Precio: $ {burgerDetalle.precio.toLocaleString({
-          style: "currency",
-          currency: "CPL"
-        })}
-        </p>
-        <div className='buttondetalle'>
-          <button onClick={() => agregarHamburguesa(burgerDetalle)}>Añadir al carrito 🍔</button>
-        </div>
+      <div className='detalle-container'>
+        <div className="card-detalle" >
+          <div className="cardimg-detalle">
+            <img src={burgerDetalle.img} alt="imagen-hamburguesa" className="imagen"></img>
+          </div>
+          <h4>
+            {burgerDetalle.nombre}
+          </h4>
+          <p className="desc"> {burgerDetalle.desc}</p>
+          <p> Precio: $ {burgerDetalle.precio.toLocaleString({
+            style: "currency",
+            currency: "CPL"
+          })}
+          </p>
+          <div className='buttondetalle'>
+            <button onClick={() => agregarHamburguesa(burgerDetalle)}>Añadir al carrito 🍔</button>
+          </div>
 
-      </div>
+        </div>
       </div>
 
       <h3 className='text-center text-warning'>COMENTARIOS</h3>
-      
+
       {
-        
-        conectado ? 
-        <form onSubmit={comentar} className='d-flex flex-column justify-content-center align-items-center p-5 w-100 '>
-        <textarea className='w-100' id="textareacomentario" onKeyUp={e => setComentario(e.target.value)} required name="comentario" placeholder='Escribe aquí tu comentario' rows={4}></textarea>
-        <div className='p-4'>
-        <button className='btn btn-primary'>Comentar</button>
-        </div>
-           </form> 
-           : null
+
+        conectado ?
+          <form onSubmit={comentar} className='d-flex flex-column justify-content-center align-items-center p-5 w-100 '>
+            <textarea className='w-100' id="textareacomentario" onKeyUp={e => setComentario(e.target.value)} required name="comentario" placeholder='Escribe aquí tu comentario' rows={4}></textarea>
+            <div className='p-4'>
+              <button className='btn btn-primary'>Comentar</button>
+            </div>
+          </form>
+          : null
       }
-     
+
 
       <div className='text-light d-flex justify-content-center flex-column align-items-center py-2 h-100 w-80'>
-      {comentarios.filter((c) => c.idBurger === Number(id)).map((coment) => (
-                                                                      <div key={coment.id} className='h-100 w-100 px-5'>
-                                                                        <p className='fs-5 text-break text-warning'><b>Comentario:</b> {coment.comentario}</p>
-                                                                        <p><b>Escrito por:</b> {coment.userName} || <span><b>Realizado el:</b> {coment.date}</span></p>
-                                                                        <hr />
-                                                                        </div>
-                                                                      ))}
-       
+        {comentarios.filter((c) => c.idBurger === Number(id)).map((coment) => (
+          <div key={coment.id} className='h-100 w-100 px-5'>
+            <p className='fs-5 text-break text-warning'><b>Comentario:</b> {coment.comentario}</p>
+            <p><b>Escrito por:</b> {coment.userName} || <span><b>Realizado el:</b> {coment.date}</span></p>
+            <hr />
+          </div>
+        ))}
+
       </div>
-     
+
     </div>
 
   )
